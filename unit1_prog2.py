2d10c005c8c7ec58c35c00e35377a2f2a66cc36c
Astha ~ <96266669+serpious@users.noreply.github.com>
2023-01-22 16:50:30 +0530

    Update unit1_prog2.py

--- unit1_prog2.py
+++ unit1_prog2.py
@@ -2,6 +2,7 @@
 # ASTHA YADAV KMC PHYSICS HONORS
 
 import math as m
+import random as ra
 
 def square(s=5):
     return s*s
@@ -27,25 +28,18 @@ def sphere(r=7):
 def cone(r=7,h=14):
     return m.pi*(1/3)*r*r*h
 
-print("\nSelect a shape and enter its parameters to get area/volume.\nAvailable shapes are : 1 square, 2 circle, 3 cube, 4 sphere, 5 rectangle, 6 cylinder, 7 cone & 8 trapezium,")
+print("\nWe are going to find area (for 2D shapes) and volumes (for 3D shapes) with random dimensions.")
+print("You can choose the shape, available shapes are : 1 square, 2 circle, 3 cube, 4 sphere, 5 rectangle, 6 cylinder, 7 cone & 8 trapezium,")
+
 n = int(input("\tPlease select the corresponding number: "))
+print("You can choose the range from which the dimensions are selected")
+s = float(input("Start of range: "))
+e = float(input("End of range: "))
 
-print("\nPlease enter the dimensions in centimeters:")
-
-a = 5
-b = 5
-r = 5
-h = 5
-
-if n<=4:
-    a = int(input("a : "))
-elif n<8:
-    h = int(input("height: "))
-    r = int(input("base/radius: "))
-else:
-    h = int(input("height: "))
-    a = int(input("top side: "))
-    b = int(input("base side: "))
+a = round(ra.uniform(s, e), 2)
+b = round(ra.uniform(s, e), 2)
+r = round(ra.uniform(s, e), 2)
+h = round(ra.uniform(s, e), 2)
 
 s = {}
 s[1] = square(a)
@@ -56,6 +50,12 @@ s[5] = rectangle(r,h)
 s[6] = cylinder(r,h)
 s[7] = cone(r,h)
 s[8] = trapezium(h,a,b)
+
+if n == 1 or n == 3: print("The randomized side is",a)
+elif n == 2 or n == 4: print("The randomized radius is",a)
+elif n == 5: print("The randomized sides are",a,b)
+elif n == 6 or n == 7: print("The randomized height is",h,"while base radius is",r)
+else: print("The randomized height is",h,"and sides are",a,b)
 
 if n == 3 or n == 4 or n == 6 or n == 7:
     print("The volume of the shape is",round(s[n],2),"cube centimeters")
